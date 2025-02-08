eeea2e007e2b099692e23c4dbd85e90eb6d51b95
KAUSHAL SAIN <kk1951999@gmail.com>
2025-02-08 23:44:23 +0530

    Updated Backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 8080 ;
 const app = express();
 app.use(cors(
   {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials:true ,
